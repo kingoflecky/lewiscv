8f01186560d5b373985510f005a0098c7a5a6738
Lewis Quinn <lewis.quinn@sainsburysbank.co.uk>
2024-07-04 13:11:35 +0100

    002: add HeaderComponent

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const HeaderComponent = () => {
+  return (
+    <>
+    <h1>Lewis Quinn</h1>
+    <h3>Software Engineer and Creative Director</h3>
+    </>
+  );
+};
+
+export default HeaderComponent;
